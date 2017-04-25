07fefca301359945fa9f9eb2c319c46665f1b374
sumitvakil <svakil@resilinc.com>
2017-04-24 19:59:47 -0700

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CarouselItemComponent } from './carousel-item/carousel-item.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselItemComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
